c03596ea1d3cba624e0c1dbe167d4f39d6f2e4bb
11ziad <ziad.n.mostafa@gmail.com>
2025-09-12 23:33:02 +0300

    last v

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -31,7 +31,7 @@ import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
 import EmailIcon from "@mui/icons-material/Email";
 import CallIcon from '@mui/icons-material/Call';
 import Popover from "@mui/material/Popover";
-
+import AddShoppingCartRoundedIcon from '@mui/icons-material/AddShoppingCartRounded';
 export default function Navbar({ toggleTheme, isDarkMode }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [unreadCount, setUnreadCount] = useState(0);
@@ -158,10 +158,11 @@ export default function Navbar({ toggleTheme, isDarkMode }) {
           <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
             <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
               <Typography variant="h6" sx={{ fontWeight: 700 }}>
-                🛍️ ZiadShop
+                 <AddShoppingCartRoundedIcon sx={{ transform: 'translateY(4px)' }}></AddShoppingCartRoundedIcon> Shoplio
               </Typography>
             </Link>
-            <IconButton onClick={handleContactOpen} sx={{ color: "#fff" }}>
+            <IconButton onClick={handleContactOpen}  sx={{
+    color: (theme) =>theme.palette.mode === "dark" ? "#fff" : "#fff",}}>
               <CallIcon />
             </IconButton>
           </Box>
